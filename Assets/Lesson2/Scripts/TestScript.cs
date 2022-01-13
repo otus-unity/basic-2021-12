db3c53a62644d4d0e729a136086a593a5044a3cb
BaranovAnton <baranov_anton@inbox.ru>
2022-01-13 22:25:53 +0300

    after lesson 2

---
+++ Assets/Lesson2/Scripts/TestScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(gameObject.name);
+
+        Instantiate(prefab);
+        
+        print(transform.parent.name);
+        print(transform.GetChild(0));
+
+        print("global position = " + transform.position);
+        print("local position = " + transform.localPosition);
+
+        /*Debug.Log("Start - Message");
+        Debug.LogWarning("Start - Warning");
+        Debug.LogError("Start - Error");*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Update - Message");
+    }
+}
