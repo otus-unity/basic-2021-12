54045daa14eaaa8e9cf4192d18687c19c2dd8051
BaranovAnton <baranov_anton@inbox.ru>
2022-02-06 10:47:13 +0300

    update gitignore, add scripts

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public enum State
+    {
+        Idle,
+        RunningToEnemy,
+        RunningFromEnemy,
+        BeginAttack,
+        Attack,
+        BeginShoot,
+        Shoot,
+    }
+
+    public enum Weapon
+    {
+        Pistol,
+        Bat,
+    }
+
+    Animator animator;
+    State state;
+
+    public Weapon weapon;
+    public Transform target;
+    public float runSpeed;
+    public float distanceFromEnemy;
+    Vector3 originalPosition;
+    Quaternion originalRotation;
+
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        state = State.Idle;
+        originalPosition = transform.position;
+        originalRotation = transform.rotation;
+    }
+
+    public void SetState(State newState)
+    {
+        state = newState;
+    }
+
+    [ContextMenu("Attack")]
+    void AttackEnemy()
+    {
+        switch (weapon) {
+            case Weapon.Bat:
+                state = State.RunningToEnemy;
+                break;
+            case Weapon.Pistol:
+                state = State.BeginShoot;
+                break;
+        }
+    }
+
+    bool RunTowards(Vector3 targetPosition, float distanceFromTarget)
+    {
+        Vector3 distance = targetPosition - transform.position;
+        if (distance.magnitude < 0.00001f) {
+            transform.position = targetPosition;
+            return true;
+        }
+
+        Vector3 direction = distance.normalized;
+        transform.rotation = Quaternion.LookRotation(direction);
+
+        targetPosition -= direction * distanceFromTarget;
+        distance = (targetPosition - transform.position);
+
+        Vector3 step = direction * runSpeed;
+        if (step.magnitude < distance.magnitude) {
+            transform.position += step;
+            return false;
+        }
+
+        transform.position = targetPosition;
+        return true;
+    }
+
+    void FixedUpdate()
+    {
+        switch (state) {
+            case State.Idle:
+                transform.rotation = originalRotation;
+                animator.SetFloat("Speed", 0.0f);
+                break;
+
+            case State.RunningToEnemy:
+                animator.SetFloat("Speed", runSpeed);
+                if (RunTowards(target.position, distanceFromEnemy))
+                    state = State.BeginAttack;
+                break;
+
+            case State.RunningFromEnemy:
+                animator.SetFloat("Speed", runSpeed);
+                if (RunTowards(originalPosition, 0.0f))
+                    state = State.Idle;
+                break;
+
+            case State.BeginAttack:
+                animator.SetTrigger("MeleeAttack");
+                state = State.Attack;
+                break;
+
+            case State.Attack:
+                break;
+
+            case State.BeginShoot:
+                animator.SetTrigger("Shoot");
+                state = State.Shoot;
+                break;
+
+            case State.Shoot:
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/CharacterAnimationEvents.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationEvents : MonoBehaviour
+{
+    Character character;
+
+    void Start()
+    {
+        character = GetComponentInParent<Character>();
+    }
+
+    void ShootEnd()
+    {
+        character.SetState(Character.State.Idle);
+    }
+
+    void AttackEnd()
+    {
+        character.SetState(Character.State.RunningFromEnemy);
+    }
+}
