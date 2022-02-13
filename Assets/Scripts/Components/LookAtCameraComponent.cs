72787b0df2bb13579c9e6b0be378ffca12e057f7
RomanAstra <roman_muratov_astu@mail.ru>
2022-02-13 12:18:52 +0300

    Lesson 11

---
+++ Assets/Scripts/Components/LookAtCameraComponent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Components
+{
+    internal sealed class LookAtCameraComponent : MonoBehaviour
+    {
+        Transform mainCameraTransform;
+
+        private void Start()
+        {
+            mainCameraTransform = Camera.main.transform;
+        }
+
+        private void Update()
+        {
+            transform.LookAt(mainCameraTransform);
+        }
+    }
+}
---
+++ Assets/Scripts/WeaponData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [CreateAssetMenu(fileName = "Weapon", menuName = "Data/Wepon")]
+    public sealed class WeaponData : ScriptableObject
+    {
+        #region New ScriptableObject
+
+        [Serializable]
+        private class WeaponsData
+        {
+            public Weapon Type;
+            public GameObject Prefab;
+        }
+
+        [SerializeField] private List<WeaponsData> _weaponsData;
+
+        #endregion
+        
+        [SerializeField] private Weapon weapon;
+
+        public Weapon Weapon => weapon;
+
+        public void CreateWeapon(Transform root)
+        {
+            
+        }
+    }
+}
