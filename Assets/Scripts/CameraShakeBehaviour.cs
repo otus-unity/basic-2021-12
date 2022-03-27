f5b0ca015b7a07b62206ca39841cf9281c585da4
RomanAstra <roman_muratov_astu@mail.ru>
2022-03-27 12:35:55 +0300

    Занятие «2D анимация. DOTween, Spine и другие инструменты»

---
+++ Assets/Scripts/CameraShakeBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace DottweenExample
+{
+    public sealed class CameraShakeBehaviour : MonoBehaviour
+    {
+        [SerializeField] private float _duration;
+        [SerializeField] private float _strength;
+        [SerializeField] private int _vibrato;
+        [Range(0.0f, 90.0f)]
+        [SerializeField] private float _randomness;
+        private Transform _cameraTransform;
+
+        private void OnValidate()
+        {
+            _cameraTransform = Camera.main.transform;
+        }
+
+        [ContextMenu("CreateShake")]
+        private void CreateShake()
+        {
+            Tweener tweener = DOTween.Shake(() => _cameraTransform.position, 
+                pos => _cameraTransform.position = pos,
+                _duration, _strength, _vibrato, _randomness);
+        }
+    }
+}
